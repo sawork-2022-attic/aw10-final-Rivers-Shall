fee74695935dd6917e65af0221d740e378d32e1d
Rivers-Shall <riversshall@qq.com>
2022-07-03 17:00:13 +0800

    + pos-products-source

---
+++ pos-products-source/src/main/java/com/micropos/posproductssource/PosProductsSourceApplication.java
@@ -0,0 +1,13 @@
+package com.micropos.posproductssource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosProductsSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosProductsSourceApplication.class, args);
+    }
+
+}
---
+++ pos-products-source/src/test/java/com/micropos/posproductssource/PosProductsSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micropos.posproductssource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosProductsSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
